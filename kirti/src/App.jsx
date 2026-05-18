6c0c4345662e705181359797b307a81b0c9322f1
niranjan <niranjanssm69@gmail.com>
2026-05-18 11:21:09 +0530

    Changes in App.jsx

--- kirti/src/App.jsx
+++ kirti/src/App.jsx
@@ -1,16 +1,10 @@
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="h-screen flex items-center justify-center bg-white">
       <h1 className="text-5xl text-cyan-400 font-bold">
-        Tailwind Working 🚀
+        Hello React Niranjan 🚀
       </h1>
-      <demo/>
-=======
-    <div>
-      <h1>Hello React Kirti</h1>
->>>>>>> 9d6e7d1720e3782bee1053a1e6dbad26268b94b1
     </div>
   )
 }
